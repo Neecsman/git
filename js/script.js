edfc4a3030e580527b54b038809295b9a39b9b34
Nikita <example@gmail.com>
2020-08-23 17:27:18 +0300

    first praktick lesson

--- js/script.js
+++ js/script.js
@@ -1,2 +1,28 @@
  "use strict";
- 
+// 1
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+console.log(numberOfFilms);
+
+
+// 2
+const presonalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    private: false
+};
+
+
+//3 
+
+let a = prompt('Один из последних просмотренных фильмов?', ''),
+    b = prompt('На сколько оцените его?', ''),
+    c = prompt('Один из последних просмотренных фильмов?', ''),
+    d = prompt('На сколько оцените его?', '');
+
+presonalMovieDB.movies[a] = b;
+presonalMovieDB.movies[c] = d;
+
+console.log(presonalMovieDB);
