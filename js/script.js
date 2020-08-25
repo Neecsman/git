6293fbd62dc0816d08677b6239b72a679e338324
Nikita <example@gmail.com>
2020-08-25 16:02:51 +0300

    add genres massive

--- js/script.js
+++ js/script.js
@@ -1,9 +1,16 @@
  "use strict";
 // 1
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+let numberOfFilms; 
 
-console.log(numberOfFilms);
+function start() {
+    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+ 
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+    }
+}
 
+start(); 
 
 // 2
 const presonalMovieDB = {
@@ -14,60 +21,57 @@ const presonalMovieDB = {
     private: false
 };
 
+// 3
 
-//3 
-//Первый цикл
-
-
-// for(let i = 0; i < 2; i++) {
-//     let a = prompt('Один из последних просмотренных фильмов?', ''),
-//         b = prompt('На сколько оцените его?', ''); 
-
-//     if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-//         presonalMovieDB.movies[a] = b;
-//         console.log('Done!');
-//     } else {
-//         console.log('error');
-//         i--;
-//     }
-
-// }
-
-//Второй вариант цикла
-
-let a, 
-    b,
-    i = 0;
-//  while (i <= 1) {
-//     a = prompt('Один из последних просмотренных фильмов?', ''),
-//     b = prompt('На сколько оцените его?', ''), 
-//     presonalMovieDB.movies[a] = b;
-//     i++;   
-//  }
-
-// Третий вариант цикла 
-
-    do {
-        a = prompt('Один из последних просмотренных фильмов?', ''),
-        b = prompt('На сколько оцените его?', ''), 
-        presonalMovieDB.movies[a] = b;
-        i++;   
-    } 
-    while (i <= 1);
-
-
-
-if (presonalMovieDB.count < 10) {
-    console.log('Вы посмотрели довольно таки мало фильмов');
-}  else if (presonalMovieDB.count >= 10 && presonalMovieDB.count < 30) {
-    console.log('Хороший список фильмов!');
-} else if (presonalMovieDB.count >= 30) {
-    console.log('Вы киноман!');
-} else {
-    console.log('Произошла ошибка');
+function rememberMyFilms() {
+    for(let i = 0; i < 2; i++) {
+        let a = prompt('Один из последних просмотренных фильмов?', ''),
+            b = prompt('На сколько оцените его?', ''); 
+    
+        if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+            presonalMovieDB.movies[a] = b;
+            console.log('Done!');
+        } else {
+            console.log('error');
+            i--;
+        }
+    }
 }
 
+rememberMyFilms();
 
-console.log(presonalMovieDB);
+
+function detectPersonalLevel () {
+    if (presonalMovieDB.count < 10) {
+        console.log('Вы посмотрели довольно таки мало фильмов');
+    }  else if (presonalMovieDB.count >= 10 && presonalMovieDB.count < 30) {
+        console.log('Хороший список фильмов!');
+    } else if (presonalMovieDB.count >= 30) {
+        console.log('Вы киноман!');
+    } else {
+        console.log('Произошла ошибка');
+    }
+}
+
+ie
+detectPersonalLevel();
+
+function showMyDB (hidden) {
+    if (!hidden) {
+       console.log(presonalMovieDB); 
+    }
+}
+showMyDB(presonalMovieDB.private);
+
+
+function writeYourGenres() {
+    for(let i = 1; i <=3; i++) {
+        presonalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`);
+    }
+}
+
+writeYourGenres();
+
+
 
 
