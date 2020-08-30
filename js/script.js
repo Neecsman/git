8fbf100dc9c8d547a21d8051e0ed263ea3d051a5
Nikita <example@gmail.com>
2020-08-30 13:03:15 +0300

    modificate our console programm

--- js/script.js
+++ js/script.js
@@ -1,77 +1,68 @@
  "use strict";
-// 1
-let numberOfFilms; 
-
-function start() {
-    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
- 
-    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
-        numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
-    }
-}
-
-start(); 
-
-// 2
+  
 const presonalMovieDB = {
-    count: numberOfFilms,
+    count: 0,
     movies: {},
     actors: {},
     genres: [],
-    private: false
-};
-
-// 3
-
-function rememberMyFilms() {
-    for(let i = 0; i < 2; i++) {
-        let a = prompt('Один из последних просмотренных фильмов?', ''),
-            b = prompt('На сколько оцените его?', ''); 
-    
-        if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-            presonalMovieDB.movies[a] = b;
-            console.log('Done!');
-        } else {
-            console.log('error');
-            i--;
+    private: false,
+    start: function () {
+        presonalMovieDB.count  = +prompt('Сколько фильмов вы уже посмотрели?', '');
+     
+        while (presonalMovieDB.count == '' || presonalMovieDB.count == null || isNaN(presonalMovieDB.count)) {
+            presonalMovieDB.count = +prompt('Сколько фильмов вы уже посмотрели?', '');
         }
+    },
+    rememberMyFilms: function () {
+        for(let i = 0; i < 2; i++) {
+            let a = prompt('Один из последних просмотренных фильмов?', ''),
+                b = prompt('На сколько оцените его?', ''); 
+        
+            if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+                presonalMovieDB.movies[a] = b;
+                console.log('Done!');
+            } else {
+                console.log('error');
+                i--;
+            }
+        }
+    },
+    detectPersonalLevel: function () {
+        if (presonalMovieDB.count < 10) {
+                console.log('Вы посмотрели довольно таки мало фильмов');
+            }  else if (presonalMovieDB.count >= 10 && presonalMovieDB.count < 30) {
+                console.log('Хороший список фильмов!');
+            } else if (presonalMovieDB.count >= 30) {
+                console.log('Вы киноман!');
+            } else {
+                console.log('Произошла ошибка');
+            }
+    },
+    showMyDB: function (hidden) {
+        if (!hidden) {
+            console.log(presonalMovieDB); 
+        }
+    },
+    toggleVisibleMyDB: function () {
+        if (presonalMovieDB.private) {
+            presonalMovieDB.private = false;
+        } else {
+            presonalMovieDB.private = true;
+        }
+    },
+    writeYourGenres: function () {
+        for(let i = 1; i <=3; i++) {
+            let ganre = prompt(`Ваш любимый жанр под номером ${i}`);
+            if (ganre == '' ||  ganre == null) {
+                console.log('Вы ввели некорретные данные!');
+                i--;
+            } else {
+                presonalMovieDB.genres[i - 1] = ganre;
+            }
+        }
+        presonalMovieDB.genres.forEach((item, i) => {
+            console.log(`Любимый жанр ${i + 1 } - это ${item}`);
+        });
     }
-}
-
-rememberMyFilms();
-
-
-function detectPersonalLevel () {
-    if (presonalMovieDB.count < 10) {
-        console.log('Вы посмотрели довольно таки мало фильмов');
-    }  else if (presonalMovieDB.count >= 10 && presonalMovieDB.count < 30) {
-        console.log('Хороший список фильмов!');
-    } else if (presonalMovieDB.count >= 30) {
-        console.log('Вы киноман!');
-    } else {
-        console.log('Произошла ошибка');
-    }
-}
-
-ie
-detectPersonalLevel();
-
-function showMyDB (hidden) {
-    if (!hidden) {
-       console.log(presonalMovieDB); 
-    }
-}
-showMyDB(presonalMovieDB.private);
-
-
-function writeYourGenres() {
-    for(let i = 1; i <=3; i++) {
-        presonalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`);
-    }
-}
-
-writeYourGenres();
-
-
-
+};
 
